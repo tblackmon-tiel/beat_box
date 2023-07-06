9c2dd155c3c7308ddbf9937aa279fab6e6d0bfc3
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-06 17:24:22 -0600

    Create the data method for node class and add tests

--- lib/node.rb
+++ lib/node.rb
@@ -1,5 +1,6 @@
 class Node
+  attr_reader :data
   def initialize(data)
-    
+    @data = data
   end
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe Node do
       expect(node).to be_an_instance_of(Node)
     end
   end
+
+  describe "#data" do
+    it "returns the value of data" do
+      node = Node.new("plop")
+
+      expect(node.data).to eq("plop")
+    end
+
+    it "can have different values of data" do
+      node = Node.new("plop")
+      node_2 = Node.new("drop")
+
+      expect(node.data).to eq("plop")
+      expect(node_2.data).to eq("drop")
+    end
+  end
 end
