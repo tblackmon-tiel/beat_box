1830716317bd4e0163400035acfa6693f45feba5
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-06 18:24:19 -0600

    add append to linkedlist

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,6 +1,12 @@
+require_relative 'node'
+
 class LinkedList
   attr_reader :head
   def initialize
     @head = nil
+  end
+
+  def append(data)
+    @head = Node.new(data)
   end
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -17,4 +17,16 @@ RSpec.describe LinkedList do
       expect(list.head).to eq(nil)
     end
   end
+
+  describe "#append" do
+    it "adds a node to the list" do
+      list = LinkedList.new
+
+      list.append("doop")
+      # require 'pry'; binding.pry
+
+      expect(list.head.data).to eq("doop")
+      expect(list.head.next_node).to eq(nil)
+    end
+  end
 end
